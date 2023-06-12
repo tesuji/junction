054e276b4d1644a7164b1af0b3a9ae3401dec1a6
Lzu Tao <taolzu@gmail.com>
2023-06-12 22:10:38 +0700

    replace macro with const fn

--- src/internals/helpers/utf16.rs
+++ src/internals/helpers/utf16.rs
@@ -1,20 +1,5 @@
-// FIXME(const_generic)
 /// Convert ASCII bytes to UTF-16 sequences.
-macro_rules! utf16s {
-    ($src:literal) => {{
-        const SRC: &[u8] = $src;
-        const N: usize = SRC.len();
-        let mut i = 0;
-        let mut dst = [0u16; N];
-        while i < N {
-            dst[i] = SRC[i] as u16;
-            i += 1;
-        }
-        dst
-    }};
-}
-
-const fn ascii_to_utf16<const N: usize>(src: [u8; N]) -> [u16; N] {
+pub const fn utf16s<const N: usize>(src: &'static [u8; N]) -> [u16; N] {
     let mut dst = [0u16; N];
     let mut i = 0;
     while i < N {
@@ -23,10 +8,3 @@ const fn ascii_to_utf16<const N: usize>(src: [u8; N]) -> [u16; N] {
     }
     dst
 }
-
-#[test]
-fn const_fn() {
-    const _: [u16; 9] = ascii_to_utf16(*br"123412341");
-}
-
-pub(crate) use utf16s;
